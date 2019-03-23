923785dc9547aefdae42c0992f076d01ef99297c
Ali Mujthaba <a.mujthaba321@gmail.com>
2019-03-23 09:08:38 +0500

    added sidebar menu

--- src/components/body/Sidebar.js
+++ src/components/body/Sidebar.js
@@ -1,10 +1,64 @@
-
 import React, { Component } from 'react';
+import { Dropdown, Icon, Input, Menu } from 'semantic-ui-react';
 
-const ViewPost = props => (
-        <div>
-            View POsts
-        </div>
-);
+export default class MenuExampleSubMenu extends Component {
+  state = {};
 
-export default ViewPost;
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name });
+
+  render() {
+    const { activeItem } = this.state;
+
+    return (
+      <Menu vertical style={{ height: '720px' }}>
+        <Menu.Item>
+          <Input placeholder="Search..." />
+        </Menu.Item>
+
+        <Menu.Item
+          name="browse"
+          color="teal"
+          active={activeItem === 'browse'}
+          onClick={this.handleItemClick}
+        >
+          Home
+          <Icon name="home" />
+        </Menu.Item>
+
+        <Menu.Item
+          name="browse"
+          color="teal"
+          active={activeItem === 'browse'}
+          onClick={this.handleItemClick}
+        >
+          Institutes
+          <Icon name="building" />
+        </Menu.Item>
+        <Menu.Item
+          name="messages"
+          active={activeItem === 'messages'}
+          onClick={this.handleItemClick}
+        >
+          Loan Schemes
+          <Icon name="money" />
+        </Menu.Item>
+        <Menu.Item
+          name="messages"
+          active={activeItem === 'messages'}
+          onClick={this.handleItemClick}
+        >
+          Scholarship Schemes
+          <Icon name="snapchat" />
+        </Menu.Item>
+
+        <Dropdown item text="Application Forms">
+          <Dropdown.Menu>
+            <Dropdown.Item icon="edit" text="Edit Profile" />
+            <Dropdown.Item icon="globe" text="Choose Language" />
+            <Dropdown.Item icon="settings" text="Account Settings" />
+          </Dropdown.Menu>
+        </Dropdown>
+      </Menu>
+    );
+  }
+}
