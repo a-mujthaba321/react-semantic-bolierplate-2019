95e45922a5f66bac3aad7aa26b5af3c1a0f8b1a9
Ali Mujthaba <a.mujthaba321@gmail.com>
2019-03-23 13:34:46 +0500

    Added Sample components with routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,28 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import { Container, Grid } from 'semantic-ui-react';
 import Sidebar from './body/Sidebar';
+import LoanScheme from './sample/LoanScheme';
 
 export default class App extends React.Component {
   render() {
     return (
-      <div>
-        <Sidebar />
-      </div>
+      <Container fluid>
+        <Grid>
+          <Grid.Row>
+            <Grid.Column width={3}>
+              <Sidebar />
+            </Grid.Column>
+            <Grid.Column width={12}>
+              <div>
+                <Switch>
+                  <Route path="/LoanSchemes" component={LoanScheme} />
+                </Switch>
+              </div>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Container>
     );
   }
 }
---
+++ src/components/sample/LoanScheme.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+export default class LoanScheme extends Component {
+  render() {
+    return <div>Loan Schemes</div>;
+  }
+}
